461b15b427278420ba2fd3b48a4c305bc6f29979
Rafael Cardoso <rafaelcardoso2021@rede.ulbra.br>
2022-10-20 14:00:40 -0300

    refactoring item venda repository find by venda codigo query

--- src/main/java/com/rafael/gvendas/gestaovendas/repository/ItemVendaRepository.java
+++ src/main/java/com/rafael/gvendas/gestaovendas/repository/ItemVendaRepository.java
@@ -2,10 +2,15 @@ package com.rafael.gvendas.gestaovendas.repository;
 
 import com.rafael.gvendas.gestaovendas.entities.ItemVenda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ItemVendaRepository extends JpaRepository<ItemVenda, Long> {
 
+    @Query("SELECT NEW com.rafael.gvendas.gestaovendas.entities.ItemVenda(" +
+            "iv.codigo, iv.quantidade, iv.precoVendido, iv.produto, iv.venda)" +
+            " FROM ItemVenda iv" +
+            " WHERE iv.venda.codigo = :codigoVenda")
     List<ItemVenda> findByVendaCodigo(Long codigoVenda);
 }
